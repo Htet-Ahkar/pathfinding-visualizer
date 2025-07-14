d552037f0b94d2e272217e731975b0b48680c463
Xero <juliuscaesar.caesar37@gmail.com>
2025-07-14 20:15:19 +0700

    add reset graph

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,7 +8,7 @@ import {
   SLEEP_TIME,
   SPEEDS,
 } from "../utils/constants";
-import { resetGrid } from "../utils/resetGrid";
+import { resetGraph, resetGrid } from "../utils/resetGrid";
 import { AlgorithmType, MazeType, SpeedType } from "../utils/types";
 import { Select } from "./Select";
 import { useSpeed } from "../hooks/useSpeed";
@@ -16,6 +16,7 @@ import { runMazeAlgorithm } from "../utils/runMazeAlgorithm";
 import { PlayButton } from "./PlayButton";
 import { runPathfindingAlgorithm } from "../utils/runPathfindingAlgorithm";
 import { animatePath } from "../utils/animatePath";
+import { GrPowerReset } from "react-icons/gr";
 
 export function Nav({
   isVisualizationRunningRef,
@@ -87,6 +88,11 @@ export function Nav({
     );
   };
 
+  const handleResetGraph = () => {
+    resetGraph({ grid });
+    setIsGraphVisualized(false);
+  };
+
   return (
     <div className="flex min-h-[4.5rem] items-center justify-center border-b px-0 shadow-gray-600 sm:px-5">
       <div className="flex w-full items-center justify-center sm:w-[52rem] lg:justify-between">
@@ -104,15 +110,36 @@ export function Nav({
               handleGenerateMaze(e.target.value as MazeType);
             }}
           />
-          <Select
-            label="Graph"
-            value={algorithm}
-            isDisabled={isDisabled}
-            options={PATHFINDING_ALGORITHMS}
-            onChange={(e) => {
-              setAlgorithm(e.target.value as AlgorithmType);
-            }}
-          />
+
+          {/* TODO */}
+          {isGraphVisualized ? (
+            <div className="flex flex-col items-start gap-1">
+              <label
+                className="ml-1 text-xs text-gray-300"
+                htmlFor="graph_reset"
+              >
+                Graph
+              </label>
+              <button
+                onClick={handleResetGraph}
+                id="graph_reset"
+                className="flex cursor-pointer items-center justify-center gap-2 rounded-md bg-gray-700 px-3 py-2 hover:bg-gray-800 active:border-0 active:ring-0"
+              >
+                <GrPowerReset className="h-5 w-5" /> Reset
+              </button>
+            </div>
+          ) : (
+            <Select
+              label="Graph"
+              value={algorithm}
+              isDisabled={isDisabled}
+              options={PATHFINDING_ALGORITHMS}
+              onChange={(e) => {
+                setAlgorithm(e.target.value as AlgorithmType);
+              }}
+            />
+          )}
+
           <Select
             label="Speed"
             value={speed}
